b5e8fcb93ece701dce2343349b71909a7a86e632
Tomek Buszewski <tomek.buszewski@gmail.com>
2017-06-17 15:47:57 +0200

    Better post list container

--- source/js/Components/PostList/index.js
+++ source/js/Components/PostList/index.js
@@ -4,19 +4,15 @@ import { Helmet } from 'react-helmet';
 import { onlyUpdateForKeys } from 'recompose';
 
 import { fetch } from '../../Actions/Content';
-import { getLastPart, getType } from '../../Services/UrlParser';
-import { orderByCriteria, filterByCriteria } from '../../Services/Database';
 
-import Card from '../../Containers/Card';
+import List from './ListContainer';
 import PaginationButtom from '../Button/PaginationButton';
 
 const PostList = ({ posts, url, title = 'Strona główna', withPagination = true }) => {
   return (
     <div>
-      {title && <Helmet>
-        <title>{title}</title>
-      </Helmet>}
-      {orderByCriteria(filterByCriteria(posts, getType(url), 'slug', getLastPart(url)), 'date').map(i => <Card key={i.id} link={i.url} title={i.title} />)}
+      {title && <Helmet><title>{title}</title></Helmet>}
+      <List posts={posts} url={url} />
       {withPagination && <PaginationButtom start={url} />}
     </div>
   );
