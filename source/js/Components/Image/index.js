bb997a572b7b317b7f8155afa178b18f1d3a6d32
Tomek Buszewski <tomekbuszewski@mbp-tomek.presspublica.pl>
2017-06-26 11:41:47 +0200

    Image component

---
+++ source/js/Components/Image/index.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Waypoint from 'react-waypoint';
+import expect from '../../Services/Expect';
+import ID from '../../Services/ID';
+
+import { BREAKPOINTS } from '../../../config';
+
+class Image extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaded: this.props.autoload
+    };
+
+    // console.log(this.getSrc());
+    this.getSrcSet();
+  }
+
+  static get defaultProps() {
+    return {
+      autoload: false,
+      forceSize: false,
+      alt: '',
+      title: ''
+    }
+  }
+
+  isSrcSet() {
+    return expect.anObject(this.props.src);
+  }
+
+  getSrc() {
+    return this.isSrcSet() ? this.props.src[0] : this.props.src;
+  }
+
+  getSrcSet() {
+    const srcset = [];
+    let i = 0;
+
+    for (const point of BREAKPOINTS) {
+      if (point.size !== 0) {
+        srcset.push({ size: point.size, src: Object.values(this.props.src)[i] });
+        // srcset.push(`${Object.values(this.props.src)[i]} ${point.size}w`);
+        ++i;
+      }
+    }
+
+    // We make sure that the array is ordered from biggest to smallest breakpoint
+    return srcset.sort((a, b) => b.size - a.size);
+  }
+
+  returnPicture() {
+    return <Waypoint onEnter={() => { console.log('a') }}><picture>
+      {this.getSrcSet().map(item => <source key={ID()} media={`(min-width: ${item.size}px)`} srcSet={item.src} />)}
+      <img src={this.getSrcSet()[0].src} />
+    </picture></Waypoint>
+  }
+
+  returnImg() {
+    return <img src={this.getSrc()} alt={this.props.alt} title={this.props.title} />;
+  }
+
+  render() {
+    return this.isSrcSet() ? this.returnPicture() : this.returnImg()
+  }
+}
+
+export default Image;
--- source/js/Containers/Card/index.js
+++ source/js/Containers/Card/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Link from '../../Components/FetchLink';
+import Image from '../../Components/Image';
 
 const Card = ({ title, link, thumbnail }) => <article className="box">
   {/*{thumbnail[0].thumbnail !== null ? console.log(thumbnail) : ''}*/}
   <h2 className="title is-5">
+    {thumbnail !== null ? <Image src={thumbnail} /> : ''}
     <Link type="post" href={link}>{title}</Link>
   </h2>
 </article>;
